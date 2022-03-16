61f207f87fd86569653206f377ff06a1092b17ec
lmitchell524 <lmitchell524@gmail.com>
2022-03-15 21:30:45 -0600

    Created form for new expenses and added event handlers for user input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// don't actually have to import react to any updated react app
+//but this shows we are using react when using JSX code
+import React from 'react';
+
 import ReactDOM from 'react-dom';
 
 import './index.css';
